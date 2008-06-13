7fe7aa359c039bb99921205f34e57cc9ce6f311f
kiba <kiba@dell.linuxdev.us.dell.com>
2008-06-13 13:07:51 -0400

    add lib/controller.rb

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,5 @@
 #The CopyPirate
-#Copyright (C) 2008 Han Dao
+#Copyright (C) 2008  Han Dao
 #
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
@@ -16,14 +16,7 @@
 
 #You can contact the author at wikipediankiba@gmail.com
 
-require 'rubygems'
-require 'rubygame'
-require 'rbgooey'
-require 'yaml'
-require 'lib/starter.rb'
-require 'lib/controller.rb'
-include Rubygame
-TTF.setup
-
-game = Starter.new
-game.play()
+class Controller
+    def initialize data
+    end
+end
