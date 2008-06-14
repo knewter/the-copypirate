95ba5c9b13e01b4c0e92dc020a340b83362a2972
kiba <kiba@dell.linuxdev.us.dell.com>
2008-06-14 14:30:55 -0400

    add lib/maplaw.rb

--- lib/maplaw.rb
+++ lib/maplaw.rb
@@ -1,5 +1,5 @@
-#The CopyPirate
-#Copyright (C) 2008 Han Dao
+#CopyPirate
+#Copyright (C) 2008  Han Dao
 #
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
@@ -9,6 +9,7 @@
 #This program is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
 #GNU General Public License for more details.
 #
 #You should have received a copy of the GNU General Public License
@@ -16,26 +17,8 @@
 
 #You can contact the author at wikipediankiba@gmail.com
 
-require 'rubygems'
-require 'rubygame'
-require 'rbgooey'
-require 'yaml'
-require 'lib/starter.rb'
-require 'lib/controller.rb'
-require 'lib/player.rb'
-require 'lib/mapengine.rb'
-require 'lib/mapdata.rb'
-require 'lib/mapobj.rb'
-require 'lib/maptile.rb'
-require 'lib/camera.rb'
-require 'lib/enemytracker.rb'
-require 'lib/zombie.rb'
-require 'lib/zombiemovement.rb'
-require 'lib/timer.rb'
-require 'lib/maplaw.rb'
-include Rubygame
-TTF.setup
-
-game = Starter.new
-game.ui()
-game.play()
+class MapLaw
+    def initialize engine
+	@engine = engine
+    end
+end
